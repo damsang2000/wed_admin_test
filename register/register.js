b19e4017bf803b346fc40bbf14b89da4939f1312
damsang2000 <damsang163@.com>
2023-03-02 11:05:56 +0700

    update page register

--- register/register.js
+++ register/register.js
@@ -3,4 +3,4 @@ console.log('Đây là trang register');
 const p = document.createElement('p');
 p.textContent = 'đây là trang đăng kí';
 
-document.getElementById('root').appendChild(p);
+document.querySelector('.container').appendChild(p);

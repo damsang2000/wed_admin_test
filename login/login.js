45b3b9dfbb0b74538d6aa5f6838904276ba9c839
damsang2000 <damsang163@.com>
2023-03-02 10:12:02 +0700

    update page login and style

--- login/login.js
+++ login/login.js
@@ -1,1 +1,7 @@
 console.log('đây là trang login');
+
+const p = document.createElement('p');
+p.textContent = 'hello';
+p.className = 'text-color';
+
+document.getElementById('root').appendChild(p);
